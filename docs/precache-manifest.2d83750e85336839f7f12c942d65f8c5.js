42baa27df43c0a3e88bd8bb32e3258e29b7180bd
dmitriy <dmitriy@coderiver.com.ua>
2018-05-03 18:05:41 +0300

    add docs

---
+++ docs/precache-manifest.2d83750e85336839f7f12c942d65f8c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb7eb278366ca664f015",
+    "url": "/js/vendor.eb7eb278.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "361d551f0e413eac6381",
+    "url": "/js/app.361d551f.js"
+  },
+  {
+    "revision": "f06a7b6917effe452c87296a11c2cff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "361d551f0e413eac6381",
+    "url": "/css/app.9b0a5c98.css"
+  }
+];
